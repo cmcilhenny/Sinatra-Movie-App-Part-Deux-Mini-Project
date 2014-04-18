addbc6a92b476bc22f5f6ade15c254f03c116867
courtney mcilhenny <github@courtneymcilhenny>
2014-04-17 21:21:44 -0700

    instance variable set-up

--- movies.rb
+++ movies.rb
@@ -11,6 +11,7 @@ configure do
 end
 
 get '/' do
+
 	erb :index
 end
 
@@ -23,13 +24,6 @@ get '/results' do
 	response = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => "#{params[:movie]}"})
 	@result = JSON.parse(response.body)
 
-	# str = "<h1>Movie List</h1>"
-
- #  result["Search"].each do |movie|
- #    str += "<br><a href=/poster/#{movie["imdbID"]}> #{movie["Title"]} - #{movie["Year"]} </a><br>"
- #  end
- #  str
-
   erb :search
 end
 
@@ -37,20 +31,6 @@ get '/poster/:imdb_id' do
 
 	response = Typhoeus.get("http://www.omdbapi.com/", :params => {:i => "#{params[:imdb_id]}"})
 	@result = JSON.parse(response.body)
-  
-  # "<h2>Movie Info</h2>
-  # 	<ul>
-  # 		<li>#{result["Title"]}</li>
-  # 		<li>#{result["Plot"]}</li>
-  # 		<li>#{result["Year"]}</li>
-  # 		<li>#{result["Rated"]}</li>
-  # 		<li>#{result["Runtime"]}</li>
-  # 		<li>#{result["Genre"]}</li>
-  # 		<li>#{result["Director"]}</li>
-  # 		<li>#{result["Actors"]}</li>
-  # 		<li><a href='http://www.imdb.com/title/#{result["imdbID"]}'>IMDB Info</a></li>
-  # 	</ul>
-  #	"<img src = #{result['Poster']}></img>"
 
   erb :show
 end
