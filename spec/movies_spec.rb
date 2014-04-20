771b460eb5cd2b41ad64ef22f045b09bf6df1cdc
courtney mcilhenny <github@courtneymcilhenny>
2014-04-20 15:13:57 -0700

    updated rspec and tweaked CSS

--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -9,5 +9,40 @@ describe "Sinatra Movie Application With OMDB API" do
     end
   end
 
+  describe "Search Results" do
+  	it "should return results for Sharknado" do
+  		get '/results?movie=sharknado'
+  		last_response.should be_ok
+  	end
   
+  	it "should return sharknado as a search results when it is searched for" do
+  		get '/results?=sharknado'
+  		last_response.should be_ok
+  		expect(last_response.body).to include("Sharknado")
+  	end
+
+  	it "should return the year sharknado was made" do
+  		get '/results?movie=sharknado'
+  		last_response.should be_ok
+  		expect(last_response.body).to include("2013")
+  	end
+  end
+
+  describe "Poster page" do
+  	it "should return a valid page for the sharknado poster" do
+  		get '/poster/tt2724064'
+  		last_response.should be_ok
+  	end
+
+  	it "should return the sharknado poster" do
+  		get '/poster/tt2724064'
+  		expect(last_response.body).to include("http://ia.media-imdb.com/images/M/MV5BOTE2OTk4MTQzNV5BMl5BanBnXkFtZTcwODUxOTM3OQ@@._V1_SX300.jpg")
+  	end
+
+  	it "should return the genre of sharknado" do
+  		get '/poster/tt2724064' 
+  		expect(last_response.body).to include("Horror")
+  	end
+
+  end
 end
